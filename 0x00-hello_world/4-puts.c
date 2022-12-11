ec65157ea154e5a69d8b71beed4e36f3b25b55e6
Humphrey Abedinigo <onoski292@gmail.com>
2022-12-11 14:05:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - prints agiven phrase
- * Return: 0 if exited proprely, non-zero otherwise 
+ *main - Entry
+ *Return: Always 0
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 }
