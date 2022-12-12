e065080a654e0f4ae0663576dc6787d68e17cd1d
Humphrey Abedinigo <onoski292@gmail.com>
2022-12-12 12:16:57 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - combination of posible numbers*
+ *Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	int a;
+
+	for (i = 48; i <= 56; i++)
+
+	{
+		for (a = 49; a <= 57; a++)
+		{
+			if (a > i)
+			{
+				putchar(i);
+				putchar(a);
+				if (i != 56 || a != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
